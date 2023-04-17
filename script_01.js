0e35665ebb326fef436e18ae19e5162bbcef4055
Joe6666666 <124160058+Joe6666666@users.noreply.github.com>
2023-04-17 12:47:40 +0200

    value deep

--- script_01.js
+++ script_01.js
@@ -32,23 +32,23 @@
 // /***** Objekte 1 Daten/Funktionen *******/
 
 
-let person = {
-                firstName:"Evtim",
-                familyName:"Trenev",
-                salary: [120000,160000],
-                permission: true,
-                sayHello:
-                        function(){
-                            return "Hallo " + this.firstName + "!";
-                        }
-            };
+// let person = {
+//                 firstName:"Evtim",
+//                 familyName:"Trenev",
+//                 salary: [120000,160000],
+//                 permission: true,
+//                 sayHello:
+//                         function(){
+//                             return "Hallo " + this.firstName + "!";
+//                         }
+//             };
 
-output(person);
-output(person.firstName);
-output(person.familyName);
-output(person.salary[1]);
-output(person.permission);
-output(person.sayHello());
+// output(person);
+// output(person.firstName);
+// output(person.familyName);
+// output(person.salary[1]);
+// output(person.permission);
+// output(person.sayHello());
 
 
 // const txt =     "Ich bin " + person.firstName + " " +
@@ -59,6 +59,28 @@ output(person.sayHello());
 //  
 
 // output(txt);
+
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+// nested object {{{}}}
+
+let baikal = {
+                value:"10m",
+                deep:{
+                        deeper:{
+                                    deepest:"Das Licht - auf 1642m!",
+                                    temperature: "4 Grad Celsius"
+                        }
+                }
+            };
+
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
+output(baikal.deep.deeper.temperature);
 
 
 
